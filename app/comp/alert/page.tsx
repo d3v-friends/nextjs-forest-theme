4e96d13788dd4813cbdba431fc5ade6d8dc58743
ciaolee87 <ciaolee87@d3v-friends.com>
2026-01-26 17:57:11 +0900

    alert 디자인 수정

--- app/comp/alert/page.tsx
+++ app/comp/alert/page.tsx
@@ -1,27 +1,41 @@
 "use client";
-import React, {useEffect} from "react";
+import React from "react";
 import {forestAlert} from "@src";
 
 export default function () {
-	useEffect(() => {
+	const onClick = () => {
 		forestAlert(
 			<>
 				<h4>forestAlert</h4>
 				<p>that's good</p>
+			</>
+		);
+	};
+
+	const onClickHeader = () => {
+		forestAlert(
+			<>
+				<p>that's good</p>
 			</>,
 			{
-				header: "Alert",
+				header: "hello",
 			}
 		);
-	}, []);
+	};
 	return (
 		<div className="grid grid-cols-1 gap-2 lg:gap-4">
 			<section>
 				<h3>Alert</h3>
 				<button
 					className="filled"
-					onClick={() => forestAlert("hello")}>
+					onClick={onClick}>
 					alert
+				</button>
+
+				<button
+					className="filled"
+					onClick={onClickHeader}>
+					header
 				</button>
 			</section>
 		</div>
