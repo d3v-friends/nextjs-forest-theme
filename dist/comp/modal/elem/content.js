710ab333b5e4a16e3f63f3a4252a1e4b63215c24
ciaolee87 <ciaolee87@d3v-friends.com>
2026-02-19 15:45:02 +0900

    1.0.24 * 모달관련 컴포넌트 디자인 수정

--- dist/comp/modal/elem/content.js
+++ dist/comp/modal/elem/content.js
@@ -1,5 +1,5 @@
 import { jsx as _jsx } from "react/jsx-runtime";
 export default function ({ children }) {
-    return _jsx("article", { className: "pl-3 pr-3 pt-2", children: children });
+    return _jsx("article", { className: "pl-3 pr-3 pt-2 pb-2", children: children });
 }
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY29udGVudC5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uLy4uLy4uL3NyYy9jb21wL21vZGFsL2VsZW0vY29udGVudC50c3giXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQU1BLE1BQU0sQ0FBQyxPQUFPLFdBQVcsRUFBQyxRQUFRLEVBQTJCO0lBQzVELE9BQU8sa0JBQVMsU0FBUyxFQUFDLGdCQUFnQixZQUFFLFFBQVEsR0FBVyxDQUFDO0FBQ2pFLENBQUMifQ==
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY29udGVudC5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uLy4uLy4uL3NyYy9jb21wL21vZGFsL2VsZW0vY29udGVudC50c3giXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQU1BLE1BQU0sQ0FBQyxPQUFPLFdBQVcsRUFBQyxRQUFRLEVBQTJCO0lBQzVELE9BQU8sa0JBQVMsU0FBUyxFQUFDLHFCQUFxQixZQUFFLFFBQVEsR0FBVyxDQUFDO0FBQ3RFLENBQUMifQ==
--- src/comp/modal/elem/content.tsx
+++ src/comp/modal/elem/content.tsx
@@ -5,5 +5,5 @@ interface Props {
 }
 
 export default function ({children}: Readonly<Partial<Props>>) {
-	return <article className="pl-3 pr-3 pt-2">{children}</article>;
+	return <article className="pl-3 pr-3 pt-2 pb-2">{children}</article>;
 }
